797938846a68d5134549dd12f1e6b345ba41b188
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-08 19:17:04 +0800

    feat: created component files, basic header and updated css

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import '../styles/Header.css'
+
+let Header = <header>
+    <h1>Who's that Pokemon!</h1>
+</header>
+
+export default Header
---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Pokemon(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
