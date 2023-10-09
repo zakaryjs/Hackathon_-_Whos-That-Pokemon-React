163b616b4179451e2dfee66681c3821387ea9c34
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-09 22:18:18 +0800

    feat: guessing bar can now can handle change and submit

--- src/components/GuessingBar.jsx
+++ src/components/GuessingBar.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
 import '../styles/GuessingBar.css'
 
-export default function GuessingBar() {
+export default function GuessingBar( {onFormSubmit} ) {
 
     const [guess, setGuess] = useState("")
 
     function formSubmit(event) {
         event.preventDefault()
+        onFormSubmit(guess)
+        console.log(guess)
     }
+
+    function handleGuessChange(event) {
+        setGuess(event.target.value)
+    }
+
 
     return (
         <form onSubmit={formSubmit}>
-            <input></input>
+            <input onChange={handleGuessChange}></input>
         </form>
     )
 
--- src/components/PokemonParent.jsx
+++ src/components/PokemonParent.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import GuessingBar from './GuessingBar';
 import Header from './Header';
 import Pokemon from './Pokemon';
 import PokemonSearch from './PokemonSearch';
 
 export default function PokemonParent() {
+
+    const [guessToCompare, setGuessToCompare] = useState("")
+
+    function handleGuessSubmit(data) {
+        setGuessToCompare(data)
+    }
+
+
   return (
     <div>
       {Header}
       <PokemonSearch />
       <Pokemon />
-      <GuessingBar />
+      <GuessingBar onFormSubmit={handleGuessSubmit}/>
     </div>
   );
 }
