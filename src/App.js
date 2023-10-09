b5e806addce82a5439af8662fd8edb87a0a0a08f
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-09 14:09:12 +0800

    feat: added PokemonSearch function, gets random pokemon from generation 1 then shows image and name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import PokemonSearch from './components/PokemonSearch';
 
 function App() {
   return (
     <div className="App" id='mainApp'>
       {Header}
+      <PokemonSearch />
     </div>
   );
 }
--- src/components/PokemonSearch.jsx
+++ src/components/PokemonSearch.jsx
@@ -1,5 +1,35 @@
+import { useContext, useEffect, useState } from "react"
+import { ApiContext } from "../contexts/ApiContext";
 
 
 export default function PokemonSearch() {
+
+    const [pokemon, setPokemon] = useState([])
+
+    const {api} = useContext(ApiContext);
+
+    function getRandomPokemonId(){
+        return Math.floor(Math.random() * 151) + 1
+    }
     
+    async function getPokemonData(targetId = getRandomPokemonId()) {
+
+        const response = await fetch(`${api}pokemon/${targetId}`)
+        const data = await response.json()
+        console.log(data)
+        setPokemon(data)
+        return data
+    }
+
+    useEffect(() => {
+        getPokemonData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <div>
+            <h2>{pokemon?.name}</h2>
+            <img src={pokemon?.sprites?.other?.home['front_default']} alt="pokemon icon" />
+        </div>
+    )
 }
