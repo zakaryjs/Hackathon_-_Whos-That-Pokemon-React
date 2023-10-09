2e1a3402402bf63fa00a0010fe69ee354a3f3bfe
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-09 14:38:27 +0800

    fix: updated context typo and re enabled strict mode for testing

--- src/contexts/AnimatedSpriteContext.js
+++ src/contexts/AnimatedSpriteContext.js
@@ -10,7 +10,7 @@ export default function AnimatedSpriteProvider ({children}) {
         <AnimatedSpriteContext.Provider value={
             {
                 sprite: animatedSpriteUrl,
-                setApi: setAnimatedSpriteUrl
+                setSprite: setAnimatedSpriteUrl
             }
         }>
             {children}
