d27c4136dfa9242bd51ac96ffc9adf04b5c6bcee
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-10 18:07:16 +0800

    feat: created buttons to change selected song, using state

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,26 +1,39 @@
+import { useState } from 'react';
 import {useAudio} from 'react-use';
 
 const Demo = () => {
+
+  const [playing, setPlaying] = useState("/audio/AzaleaTown.mp3")
+
+  // eslint-disable-next-line no-unused-vars
   const [audio, state, controls, ref] = useAudio({
-    src: '/audio/AzaleaTown.mp3',
+    src: playing,
     autoPlay: false,
   });
+
+  function Lacunosa() {
+    setPlaying("/audio/LacunosaTown.mp3")
+  }
+
+  function Azalea() {
+    setPlaying("/audio/AzaleaTown.mp3")
+  }
 
   return (
     <div>
       {audio}
-      <button onClick={controls.pause}>Pause</button>
+      <button onClick={Lacunosa}>Set Song: Lacunosa</button>
+      <button onClick={Azalea}>Set Song: Azalea</button>
+      <br/>
       <button onClick={controls.play}>Play</button>
+      <button onClick={controls.pause}>Pause</button>
       <br/>
       <button onClick={controls.mute}>Mute</button>
-      <button onClick={controls.unmute}>Un-mute</button>
+      <button onClick={controls.unmute}>Unmute</button>
       <br/>
       <button onClick={() => controls.volume(.1)}>Volume: 10%</button>
       <button onClick={() => controls.volume(.5)}>Volume: 50%</button>
       <button onClick={() => controls.volume(1)}>Volume: 100%</button>
-      <br/>
-      <button onClick={() => controls.seek(state.time - 5)}>-5 sec</button>
-      <button onClick={() => controls.seek(state.time + 5)}>+5 sec</button>
     </div>
   );
 };
