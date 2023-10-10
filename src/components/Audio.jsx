d53d851972b9d344355c05750fc147249a6b9712
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-10 16:17:02 +0800

    feat: used docs to create demo music player, added pokemon music

---
+++ src/components/Audio.jsx
@@ -0,0 +1,29 @@
+import {useAudio} from 'react-use';
+
+const Demo = () => {
+  const [audio, state, controls, ref] = useAudio({
+    src: 'https://fi.zophar.net/soundfiles/nintendo-ds-2sf/pokemon-black-white/404%20-%20Kagome%20Town.mp3',
+    autoPlay: false,
+  });
+
+  return (
+    <div>
+      {audio}
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <button onClick={controls.pause}>Pause</button>
+      <button onClick={controls.play}>Play</button>
+      <br/>
+      <button onClick={controls.mute}>Mute</button>
+      <button onClick={controls.unmute}>Un-mute</button>
+      <br/>
+      <button onClick={() => controls.volume(.1)}>Volume: 10%</button>
+      <button onClick={() => controls.volume(.5)}>Volume: 50%</button>
+      <button onClick={() => controls.volume(1)}>Volume: 100%</button>
+      <br/>
+      <button onClick={() => controls.seek(state.time - 5)}>-5 sec</button>
+      <button onClick={() => controls.seek(state.time + 5)}>+5 sec</button>
+    </div>
+  );
+};
+
+export default Demo
