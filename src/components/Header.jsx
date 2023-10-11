51757147a82fef03fa3ff5fe69957e1b1f155f36
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-11 21:03:10 +0800

    feat: changed font and updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import '../styles/Header.css'
 
 let Header = <header>
-    <h1>Who's that Pokemon!</h1>
+    <h1>Who's that Pokemon?</h1>
+    <h2>A game to test your Pokemon knowledge!</h2>
 </header>
 
 export default Header
